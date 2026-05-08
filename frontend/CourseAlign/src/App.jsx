954db9be00c3e14b4f1f7b15769fef47f5b51c22
matheusmz22 <matheusmzucchi@gmail.com>
2026-05-08 15:14:53 -0700

    Added React Query for state management

--- frontend/CourseAlign/src/App.jsx
+++ frontend/CourseAlign/src/App.jsx
@@ -1,22 +1,41 @@
 import {useState} from "react";
 import AppLayout from "./components/layout/AppLayout";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000, - 1 min
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   const [courses, setCourses] = useState([]);
   const [earliestStart, setEarliestStart] = useState("08:00");
   const [latestEnd, setLatestEnd] = useState("22:00");
+  const [schedules, setSchedules] = useState([]);
+  const [selectedSchedule, setSelectedSchedule] = useState(null);
 
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <AppLayout
+        cpLayout
         courses={courses}
         setCourses={setCourses}
         earliestStart={earliestStart}
         setEarliestStart={setEarliestStart}
         latestEnd={latestEnd}
         setLatestEnd={setLatestEnd}
+        schedules={schedules}
+        setSchedules={setSchedules}
+        selectedSchedule={selectedSchedule}
+        setSelectedSchedule={setSelectedSchedule}
       />
-    </div>
+    </QueryClientProvider>
   );
 }
 
