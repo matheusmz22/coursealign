d0e92b7a537780b470a7dcb720c51da8b3aef3f6
Greta G <143027784+GretaGuenther@users.noreply.github.com>
2026-05-09 19:33:12 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://coursealign.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
